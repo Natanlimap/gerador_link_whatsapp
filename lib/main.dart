dc235d8af2173ba0232cf8e266237897884ec551
natanlimap <natanlimap@gmail.com>
2020-11-30 09:51:06 -0300

    esqueleto pronto

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:html';
+
+import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,92 +25,87 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  TextEditingController numeroDeCelular = TextEditingController();
+  TextEditingController mensagem = TextEditingController();
+  gerarLink() {
+    return "https://api.whatsapp.com/send?phone=" +
+        numeroDeCelular.text +
+        "&text=" +
+        converterMensagem();
+  }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  converterMensagem() {
+    String mensagemConvertida;
+    mensagemConvertida = mensagem.text.replaceAll(" ", "%20");
+    return mensagemConvertida;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    var size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        backgroundColor: Colors.blue,
+        body: Center(
+            child: Container(
+          width: size.width * 0.3,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TextFormField(
+                controller: numeroDeCelular,
+                decoration: InputDecoration(
+                    focusedBorder: OutlineInputBorder(),
+                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: "Digite o numero de telefone"),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                controller: mensagem,
+                minLines: 4,
+                maxLines: 4,
+                decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(),
+                    focusedBorder: OutlineInputBorder(),
+                    labelStyle: TextStyle(color: Colors.white),
+                    labelText: "Digite a sua mensagem"),
+              ),
+              SizedBox(height: 20),
+              RaisedButton(
+                padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                color: Colors.yellow,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(18.0),
+                ),
+                child: Text(
+                  "Gerar link",
+                  style: TextStyle(color: Colors.black),
+                ),
+                onPressed: () {
+                  FlutterClipboard.copy(gerarLink())
+                      .then((value) => print("copiei"));
+                },
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "SEU LINK FOI GERADO, COLAR ONDE DESEJAR",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        )));
   }
 }
